48b3103f490ac4abfa5a1992c0f300de4f82679e
Sundeep <sundeep.kaler@gmail.com>
2023-09-09 02:29:18 -0400

    updated event listener

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Your code here
 const dodger = document.getElementById("dodger");
 
+
+
 function moveDodgerLeft() {
     const leftNumbers = dodger.style.left.replace("px", "");
     const left = parseInt(leftNumbers, 10);
@@ -18,3 +20,12 @@ function moveDodgerLeft() {
       dodger.style.left = `${left + 1}px`;
     }
   }
+
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+    else if (event.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+  });
